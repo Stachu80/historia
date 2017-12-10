d1e4336bbcc1341faf43ff3945e3c45d8ba147f1
Stachu80 <michal.stasinski@poczta.fm>
2017-12-10 09:32:16 +0100

    hhtp servis

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  isHidden = false;
+  colorHeader = 'color1';
 
   ngOnInit() {
   }
